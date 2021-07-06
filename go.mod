8e9b33c3ce0160ddf4fda168ef29c9aa52b112c4
Akeem Philbert <akeem.philbert@wepala.com>
2021-07-06 09:07:50 -0400

    Added json meta tags

--- go.mod
+++ go.mod
@@ -3,16 +3,16 @@ module github.com/wepala/blog-aggregator-api
 go 1.16
 
 require (
-	github.com/cucumber/godog v0.11.0 // indirect
-	github.com/cucumber/messages-go/v10 v10.0.3 // indirect
-	github.com/labstack/echo v3.3.10+incompatible
-	github.com/labstack/echo/v4 v4.3.0 // indirect
-	github.com/labstack/gommon v0.3.0 // indirect
-	github.com/matryer/moq v0.2.1 // indirect
-	github.com/wepala/blog-aggregator-module v0.0.0-20210619203441-784ad943df8f // indirect
-	github.com/wepala/go-testhelpers v0.0.0-20200715110105-55c57c235b75 // indirect
-	github.com/wepala/weos v0.0.7-0.20210607144120-0006285c4ee3 // indirect
-	github.com/wepala/weos-controller v0.0.0-20210619134533-2c471023eb8a // indirect
-	gorm.io/driver/sqlite v1.1.4 // indirect
-	gorm.io/gorm v1.21.10 // indirect
+	github.com/cucumber/godog v0.11.0
+	github.com/cucumber/messages-go/v10 v10.0.3
+	github.com/labstack/echo/v4 v4.3.0
+	github.com/labstack/gommon v0.3.0
+	github.com/lestrrat-go/jwx v1.2.1 // indirect
+	github.com/wepala/blog-aggregator-module v0.0.0-20210619203441-784ad943df8f
+	github.com/wepala/go-testhelpers v0.0.0-20200715110105-55c57c235b75
+	github.com/wepala/weos v0.0.7-0.20210607144120-0006285c4ee3
+	github.com/wepala/weos-controller v0.0.0-20210625160511-6d256ddaef1a
+	golang.org/x/sys v0.0.0-20210611083646-a4fc73990273 // indirect
+	gorm.io/driver/sqlite v1.1.4
+	gorm.io/gorm v1.21.10
 )
--- src/projections.go
+++ src/projections.go
@@ -16,76 +16,76 @@ import (
 
 type Projection interface {
 	weos.Projection
-	GetBlogByID (id string) (*Blog, error)
+	GetBlogByID(id string) (*Blog, error)
 	GetBlogByURL(url string) (*Blog, error)
-	GetPosts (page int, limit int, query string, sortOptions map[string]string, filterOptions map[string]interface{}) ([]*Post, int64, error)
+	GetPosts(page int, limit int, query string, sortOptions map[string]string, filterOptions map[string]interface{}) ([]*Post, int64, error)
 }
 
 type Blog struct {
 	gorm.Model
-	ID string `gorm:"primarykey"`
-	Title string `json:"title,omitempty"`
-	Description string `json:"description,omitempty"`
-	URL string `json:"url,omitempty"`
-	FeedURL string`json:"feedUrl,omitempty"`
-	Authors []*Author `json:"authors,omitempty"`
-	Posts []*Post `json:"posts,omitempty"`
+	ID          string    `gorm:"primarykey"`
+	Title       string    `json:"title,omitempty"`
+	Description string    `json:"description,omitempty"`
+	URL         string    `json:"url,omitempty"`
+	FeedURL     string    `json:"feedUrl,omitempty"`
+	Authors     []*Author `json:"authors,omitempty"`
+	Posts       []*Post   `json:"posts,omitempty"`
 }
 
 type Author struct {
 	gorm.Model
-	Name string
-	Email string
+	Name   string
+	Email  string
 	BlogID string `json:"blogId"`
 }
 
 type Post struct {
 	gorm.Model
-	ID string `gorm:"primarykey"`
-	Title string
-	Description string
-	Content string
-	BlogID string `json:"blogId"`
-	Categories []*Category `json:"categories,omitempty" gorm:"many2many:post_categories;"`
-	Published string `json:"published"`
+	ID          string      `gorm:"primarykey"`
+	Title       string      `json:"title"`
+	Description string      `json:"description"`
+	Content     string      `json:"content"`
+	BlogID      string      `json:"blogId"`
+	Categories  []*Category `json:"categories,omitempty" gorm:"many2many:post_categories;"`
+	Published   string      `json:"published"`
 	PublishDate time.Time
-	Views int
+	Views       int `json:"views"`
 }
 
 type Category struct {
 	gorm.Model
-	Title string
+	Title       string
 	Description string
-	Posts []*Post `json:"posts,omitempty" gorm:"many2many:post_categories;"`
+	Posts       []*Post `json:"posts,omitempty" gorm:"many2many:post_categories;"`
 }
 
 type GORMProjection struct {
-	db *gorm.DB
-	logger weos.Log
+	db              *gorm.DB
+	logger          weos.Log
 	migrationFolder string
 }
 
-func (p *GORMProjection) Persist( entities []weos.Entity) error {
+func (p *GORMProjection) Persist(entities []weos.Entity) error {
 	return nil
 }
 
-func (p *GORMProjection) Remove (entities []weos.Entity) error {
+func (p *GORMProjection) Remove(entities []weos.Entity) error {
 	return nil
 }
 
-func (p *GORMProjection) GetBlogByID (id string) (*Blog, error) {
-	return nil,nil
+func (p *GORMProjection) GetBlogByID(id string) (*Blog, error) {
+	return nil, nil
 }
 
-func (p *GORMProjection) GetBlogs () ([]*Blog, error) {
-	return nil,nil
+func (p *GORMProjection) GetBlogs() ([]*Blog, error) {
+	return nil, nil
 }
 
 func (p *GORMProjection) GetBlogByURL(url string) (*Blog, error) {
 	var blog *Blog
-	if err := p.db.Debug().Preload(clause.Associations).First(&blog,"url = ? OR feed_url = ?",url, url).Error; err != nil {
+	if err := p.db.Debug().Preload(clause.Associations).First(&blog, "url = ? OR feed_url = ?", url, url).Error; err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return nil,fmt.Errorf("blog '%s' not found",url)
+			return nil, fmt.Errorf("blog '%s' not found", url)
 		}
 	}
 	if p.db.Error != nil {
@@ -93,24 +93,25 @@ func (p *GORMProjection) GetBlogByURL(url string) (*Blog, error) {
 	}
 	return blog, nil
 }
+
 //GetPosts get all the posts in the aggregator
-func (p *GORMProjection) GetPosts (page int, limit int, query string, sortOptions map[string]string, filterOptions map[string]interface{}) ([]*Post, int64, error) {
+func (p *GORMProjection) GetPosts(page int, limit int, query string, sortOptions map[string]string, filterOptions map[string]interface{}) ([]*Post, int64, error) {
 	var posts []*Post
 	var count int64
-	result := p.db.Debug().Preload("Categories").Scopes(filter(filterOptions),paginate(page,limit),sort(sortOptions)).Find(&posts).Offset(-1).Distinct("posts.id").Count(&count)
-	return posts,count,result.Error
+	result := p.db.Debug().Preload("Categories").Scopes(filter(filterOptions), paginate(page, limit), sort(sortOptions)).Find(&posts).Offset(-1).Distinct("posts.id").Count(&count)
+	return posts, count, result.Error
 }
 
 func sort(order map[string]string) func(db *gorm.DB) *gorm.DB {
 	return func(db *gorm.DB) *gorm.DB {
-		for key,value := range order {
+		for key, value := range order {
 			//only support certain values since GORM doesn't protect the order function https://gorm.io/docs/security.html#SQL-injection-Methods
 			if (value != "asc" && value != "desc" && value != "") || (key != "views" && key != "publishDate") {
 				return db
 			}
-			db.Order(key+" "+value)
+			db.Order(key + " " + value)
 		}
-		
+
 		return db
 	}
 }
@@ -118,7 +119,7 @@ func sort(order map[string]string) func(db *gorm.DB) *gorm.DB {
 func category(categoryValue interface{}) func(db *gorm.DB) *gorm.DB {
 	return func(db *gorm.DB) *gorm.DB {
 		if category, ok := categoryValue.(string); ok {
-			db.Joins("left join post_categories on post_id = posts.id").Joins("left join categories on categories.id = post_categories.category_id").Where("categories.title = ?",category)
+			db.Joins("left join post_categories on post_id = posts.id").Joins("left join categories on categories.id = post_categories.category_id").Where("categories.title = ?", category)
 		}
 		return db
 	}
@@ -131,23 +132,23 @@ func publishDate(startDateValue interface{}, endDateValue interface{}) func(db *
 			var endDate time.Time
 			var err error
 			if sdv, ok := startDateValue.(string); ok {
-				startDate, err = time.Parse("01/02/06",sdv)
+				startDate, err = time.Parse("01/02/06", sdv)
 				if err != nil {
 					return db
 				}
 			}
-	
+
 			if edv, ok := endDateValue.(string); ok {
-				endDate, err = time.Parse("01/02/06",edv)
-				endDate = time.Date(endDate.Year(),endDate.Month(),endDate.Day(),23,59,59,endDate.Nanosecond(),endDate.Location())
+				endDate, err = time.Parse("01/02/06", edv)
+				endDate = time.Date(endDate.Year(), endDate.Month(), endDate.Day(), 23, 59, 59, endDate.Nanosecond(), endDate.Location())
 				if err != nil {
 					return db
 				}
 			}
-	
+
 			db.Where("publish_date BETWEEN ? AND ?", startDate, endDate)
 		}
-		
+
 		return db
 	}
 }
@@ -155,24 +156,24 @@ func publishDate(startDateValue interface{}, endDateValue interface{}) func(db *
 func filter(filter map[string]interface{}) func(db *gorm.DB) *gorm.DB {
 	return func(db *gorm.DB) *gorm.DB {
 		if filter != nil {
-			if categoryValue,ok := filter["category"];ok {
+			if categoryValue, ok := filter["category"]; ok {
 				db.Scopes(category(categoryValue))
-				delete(filter,"category")
+				delete(filter, "category")
 			}
 
 			var startDateValue interface{}
 			var endDateValue interface{}
 			var ok bool
 
-			if startDateValue,ok = filter["start_date"];ok {
-				delete(filter,"start_date")
+			if startDateValue, ok = filter["start_date"]; ok {
+				delete(filter, "start_date")
 			}
 
-			if endDateValue,ok = filter["end_date"];ok {
-				delete(filter,"end_date")
+			if endDateValue, ok = filter["end_date"]; ok {
+				delete(filter, "end_date")
 			}
 			if startDateValue != nil && endDateValue != nil {
-				db.Scopes(publishDate(startDateValue,endDateValue))
+				db.Scopes(publishDate(startDateValue, endDateValue))
 			}
 			return db.Where(filter)
 		}
@@ -194,15 +195,14 @@ func paginate(page int, limit int) func(db *gorm.DB) *gorm.DB {
 	}
 }
 
-
 func (p *GORMProjection) GetEventHandler() weos.EventHandler {
-	return func (event weos.Event) {
+	return func(event weos.Event) {
 		switch event.Type {
 		case blogaggregatormodule.BLOG_ADDED:
 			var blog *Blog
-			err := json.Unmarshal(event.Payload,&blog)
+			err := json.Unmarshal(event.Payload, &blog)
 			if err != nil {
-				p.logger.Errorf("error unmarshalling event '%s'",err)
+				p.logger.Errorf("error unmarshalling event '%s'", err)
 			}
 			db := p.db.Create(blog)
 			if db.Error != nil {
@@ -210,9 +210,9 @@ func (p *GORMProjection) GetEventHandler() weos.EventHandler {
 			}
 		case blogaggregatormodule.BLOG_UPDATED:
 			var blog *Blog
-			err := json.Unmarshal(event.Payload,&blog)
+			err := json.Unmarshal(event.Payload, &blog)
 			if err != nil {
-				p.logger.Errorf("error unmarshalling event '%s'",err)
+				p.logger.Errorf("error unmarshalling event '%s'", err)
 			}
 			blog.ID = event.Meta.EntityID
 			db := p.db.Model(blog).Updates(blog)
@@ -221,35 +221,36 @@ func (p *GORMProjection) GetEventHandler() weos.EventHandler {
 			}
 		case blogaggregatormodule.AUTHOR_CREATED:
 			var author *Author
-			err := json.Unmarshal(event.Payload,&author)
+			err := json.Unmarshal(event.Payload, &author)
 			if err != nil {
-				p.logger.Errorf("error unmarhsalling event '%s'",err)
+				p.logger.Errorf("error unmarhsalling event '%s'", err)
 			}
 			db := p.db.Create(author)
 			if db.Error != nil {
-				p.logger.Errorf("error creating author '%s'",err)
+				p.logger.Errorf("error creating author '%s'", err)
 			}
 		case blogaggregatormodule.POST_CREATED:
 			var post *Post
-			err := json.Unmarshal(event.Payload,&post)
+			err := json.Unmarshal(event.Payload, &post)
 			if err != nil {
-				p.logger.Errorf("error unmarshalling event '%s'",err)
+				p.logger.Errorf("error unmarshalling event '%s'", err)
 			}
 			post.ID = blogaggregatormodule.GenerateID()
-			post.PublishDate, err = time.Parse("Mon, 2 Jan 2006 15:04:05 -0700",post.Published)
+			post.PublishDate, err = time.Parse("Mon, 2 Jan 2006 15:04:05 -0700", post.Published)
 			if err != nil {
-				p.logger.Errorf("error parsing publish date '%s'",err)
+				p.logger.Errorf("error parsing publish date '%s'", err)
 			}
 			db := p.db.Create(post)
 			if db.Error != nil {
-				p.logger.Errorf("error creating post '%s'",err)
+				p.logger.Errorf("error creating post '%s'", err)
 			}
 		}
 	}
 }
+
 //runs migrations
 func (p *GORMProjection) Migrate(ctx context.Context) error {
-	err := p.db.AutoMigrate(&Blog{},&Post{},&Author{},&Category{})
+	err := p.db.AutoMigrate(&Blog{}, &Post{}, &Author{}, &Category{})
 	if err != nil {
 		return err
 	}
@@ -259,10 +260,9 @@ func (p *GORMProjection) Migrate(ctx context.Context) error {
 
 func NewProjection(application weos.Application) (*GORMProjection, error) {
 	projection := &GORMProjection{
-		db: application.DB(),
+		db:     application.DB(),
 		logger: application.Logger(),
 	}
 	application.AddProjection(projection)
 	return projection, nil
 }
-
